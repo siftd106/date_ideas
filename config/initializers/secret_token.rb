370a76e6f14201472342a4e439a2b78ca351c51b
Joel Bouwkamp <joel@joel-lt-2.local>
2014-01-18 13:46:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DateIdeas::Application.config.secret_key_base = '0c5fefa561eab99caa312ed02bb7ed79682f90253f3ff479033a0fbba1da1f5f493a4bea31746fbb974216a4512343fa3ed458643bbf37c01941799fce0a80be'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DateIdeas::Application.config.session_store :cookie_store, key: '_date_ideas_session'
