df7b73f153445cc639be7e25d9dbb3f12f2b43f1
Joel Bouwkamp <joel@joel-lt-2.local>
2014-01-18 18:52:20 -0500

    add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Location.create(city_name: 'Grand Rapids')
 
+Price.create(price_range: 'Free')
+Price.create(price_range: '$0 - $20')
+Price.create(price_range: '$20 - $50')
+Price.create(price_range: '$50+')
+
+Duration.create(duration: '0 to 1 hours')
+Duration.create(duration: '1 to 2 hours')
+Duration.create(duration: '2 to 3 hours')
+Duration.create(duration: '4 + hours')
+
+Stops.create(stop_range: '1')
+Stops.create(stop_range: '2')
+Stops.create(stop_range: '3')
+Stops.create(stop_range: '4+')
