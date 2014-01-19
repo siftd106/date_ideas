4bbff6626450fb1d27aab74c9b95c657ed59d530
Joel Bouwkamp <joel@joel-lt-2.local>
2014-01-18 19:57:30 -0500

    add the locations controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ DateIdeas::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  resources :location, only: [:show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
