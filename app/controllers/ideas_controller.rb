dc9b77f517b045b588b52f70d048c17993065a93
Joel Bouwkamp <joel@joel-lt-2.local>
2014-01-19 09:45:39 -0500

    start adding the the new page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,6 @@
 class IdeasController < ApplicationController
   def index
-    
+    @include_add_link = true
     if params[:next]
       @idea = Idea.find(params[:prev_idea_id].to_i + 1)
     elsif params[:prev]
@@ -11,6 +11,16 @@ class IdeasController < ApplicationController
 
     @idea.id > 1 ? @include_prev_link = true : @include_prev_link = false
     @idea.id == Idea.last.id ? @include_next_link = false : @include_next_link = true
-    
+  end
+
+  def new
+    @include_add_link = false
+    @idea = Idea.new
+  end
+
+  def create
+    @idea = Idea.create(params[:idea])
+
+    redirect_to root_url
   end
 end
