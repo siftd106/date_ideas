55d0b553ad10a570312aba25f6b9f321578222af
Joel Bouwkamp <joel@joel-lt-2.router>
2014-01-28 21:10:42 -0500

    fix the find ideas button on the home page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,17 +1,8 @@
 class IdeasController < ApplicationController
-  # def index
-  #   @include_add_link = true
-  #   if params[:next]
-  #     @idea = Idea.find(params[:prev_idea_id].to_i + 1)
-  #   elsif params[:prev]
-  #     @idea = Idea.find(params[:prev_idea_id].to_i - 1)
-  #   else
-  #     @idea = Idea.first
-  #   end
-
-  #   @idea.id > 1 ? @include_prev_link = true : @include_prev_link = false
-  #   @idea.id == Idea.last.id ? @include_next_link = false : @include_next_link = true
-  # end
+  def index
+    @idea = Idea.all.order(created_at: :desc).limit(1).first
+    render :show
+  end
 
   def show
     @idea = Idea.find params[:id]
