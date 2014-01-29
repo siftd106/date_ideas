045f4cc236a510443908bd17761667bc48a3504e
Joel Bouwkamp <joel@joel-lt-2.router>
2014-01-28 21:07:11 -0500

    add a couple of comments

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,10 +4,12 @@ class Idea < ActiveRecord::Base
   belongs_to :destination
   belongs_to :duration
 
+  # find the idea the was created directly before this idea
   def next_idea
     Idea.where(["created_at < ?", created_at]).order(created_at: :desc).limit(1).first
   end
 
+  # find the idea the was created directly after this idea
   def prev_idea
     Idea.where(["created_at > ?", created_at]).order(created_at: :asc).limit(1).first
   end
